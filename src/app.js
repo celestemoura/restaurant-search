194b29793702ec0abb9a82d2d2a1b40ee59a089b
Celeste <celestemouras@gmail.com>
2024-04-22 17:28:38 +0200

    refactoring, making sure spinner is displayed while waiting

--- src/app.js
+++ src/app.js
@@ -1,25 +1,27 @@
+let loadingSpinner = document.getElementById("loading-spinner");
+
 function handleSubmit(event) {
   event.preventDefault();
   let postcode = document.querySelector("#postcode-input").value;
+
+  loadingSpinner.classList.remove("d-none");
 
   let formattedPostcode = "";
   for (let i = 0; i < postcode.length; i++) {
     if (postcode[i] !== " ") {
       formattedPostcode += postcode[i];
     }
-}
+  }
 
   if (formattedPostcode.length > 0) {
     search(formattedPostcode);
   } else {
     handleErrors();
   }
-}   
+}
 
 let postcodeSearch = document.querySelector("form");
 postcodeSearch.addEventListener("submit", handleSubmit);
-
-let loadingSpinner = document.getElementById("loading-spinner");
 
 function search(postcode) {
   let apiUrl = `https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}`;
@@ -27,37 +29,34 @@ function search(postcode) {
   // make sure loading spinner is present
   loadingSpinner.classList.remove("d-none");
 
-  axios.get(apiUrl).then((response) => {
-    display(response);
-  })
-  .catch(() => {
-    handleErrors();
-  })
-  .finally(() => {
-    loadingSpinner.classList.add("d-none");
-  })
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      display(response);
+    })
+    .catch((error) => {
+      handleErrors();
+    })
+    .finally(() => {
+      loadingSpinner.classList.add("d-none");
+    });
 }
 
 function handleErrors() {
   alert("Please enter a valid UK postcode");
+  window.location.reload();
 }
 
 function display(response) {
   let searchResults = response.data.restaurants;
-  console.log(response.data.metaData.location)
+  let searchResultsRow = document.querySelector(".search-results");
 
   if (response.data.metaData.location == null) {
     handleErrors();
-  } else if (
-    response.data.metaData.resultCount == 0
-  ) {
-    let searchResultsCol = document.querySelector(".search-results");
-    let searchResultsHTML = `<h1>No restaurants near you... ☹️ </h1>`;
-
-    searchResultsHTML += `</div>`;
-    searchResultsCol.innerHTML = searchResultsHTML;
+  } else if (response.data.metaData.resultCount == 0) {
+    let searchResultsHTML = `<div class="col-12"><h1>No restaurants near you... ☹️ </h1></div></div>`;
+    searchResultsRow.innerHTML = searchResultsHTML;
   } else {
-    let searchResultsCol = document.querySelector(".search-results");
     let searchResultsHTML = `<h1>Restaurants near you</h1>`;
 
     searchResults.forEach(function (restaurant, index) {
@@ -74,11 +73,10 @@ function display(response) {
           <p class="address">${restaurant.address.firstLine}<br />
           ${restaurant.address.postalCode}<br />
           ${restaurant.address.city}</p>
-          </div>`;
+          </div></div>`;
       }
     });
 
-    searchResultsHTML += `</div>`;
-    searchResultsCol.innerHTML = searchResultsHTML;
+    searchResultsRow.innerHTML = searchResultsHTML;
   }
 }
